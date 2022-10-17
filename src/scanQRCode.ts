e6232acacccad631932d530c4e2de7dc1adb745e
Snehal Somalraju <snehal.somalraju@oakstreethealth.com>
2022-10-17 16:43:40 -0500

    Fix compiler errors in scanQRCode.ts

--- src/scanQRCode.ts
+++ src/scanQRCode.ts
@@ -1,7 +1,7 @@
 var QRCode = require('qrcode')
 var canvas = document.getElementById('canvas')
 
-QRCode.toCanvas(canvas, 'sample text', function (error) {
+QRCode.toCanvas(canvas, 'sample text', function (error: any) {
   if (error) console.error(error)
   console.log('success!');
 })
