39c214cb5f610aaf388754b2ec18e8e62c6cf20e
Vince Schramer <885196+scvnc@users.noreply.github.com>
2022-10-17 18:13:12 -0500

    feat: build the site and MFE in one go, together

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,26 @@ import path from "path";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import { CSS_KEY } from "./src/const";
 
+const { BUILD_MFE } = process.env;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), cssInjectedByJsPlugin({ styleId: CSS_KEY })],
   build: {
+    outDir: BUILD_MFE ? "dist/mfe" : "dist",
     sourcemap: true,
     rollupOptions: {
+      // input: {
+      // main: path.resolve(__dirname, "index.html"),
+      // },
       external: builtinModules, // https://github.com/vitejs/vite/issues/5676#issuecomment-1103710522
     },
-    lib: {
-      name: "mfe-beam",
-      entry: path.resolve(__dirname, "src/lib.ts"),
-      formats: ["es"],
-    },
+    lib: BUILD_MFE
+      ? {
+          name: "mfe-beam",
+          entry: path.resolve(__dirname, "src/lib.ts"),
+          formats: ["es"],
+        }
+      : undefined,
   },
 });
